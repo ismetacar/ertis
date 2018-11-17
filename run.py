110c7f46f7d29a57f3d52cff62154a3adaa407ca
Ismet Acar Dogan <ismetacar@Ismets-MacBook-Air.local>
2018-11-17 21:00:57 +0300

    config fixed

--- run.py
+++ run.py
@@ -1,26 +1,42 @@
 import json
+import optparse
 import os
 import traceback
 
 from flask import Response
 
+from confs.development import development_config
+from confs.local import local_config
+from confs.production import production_config
 from src import create_app
 from src.resources.security import ErtisSecurityManager
 from src.utils.errors import ErtisError
 from src.utils.json_helpers import bson_to_json, parse_boolean
 
+CONFIG_LOOKUP = {
+    'local': local_config,
+    'development': development_config,
+    'production': production_config
+}
+
+parser = optparse.OptionParser()
+
+parser.add_option("--config", default="local")
+
 
 def config_settings():
-    config_file_path = os.path.abspath(os.path.join(os.path.dirname(__file__), "confs/local.py"))
-    with open(config_file_path, 'r') as f:
-        settings = json.loads(f.read().split('=')[1], object_hook=parse_boolean)
-    f.close()
-    return settings
+    options, _ = parser.parse_args()
+    return CONFIG_LOOKUP[options.config]
 
 
 settings = config_settings()
+
 app = create_app(settings)
 app.security_manager = ErtisSecurityManager(app.db)
+
+app.debug = settings['debug']
+app.secret_key = settings['application_secret']
+app.port = settings['port']
 
 if settings.get('error_handler', False):
 
@@ -49,4 +65,4 @@ if settings.get('error_handler', False):
         )
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(port=app.port)
