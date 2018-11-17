de57b1697ac4ad4caac10a20b3036805a98154ab
ismetacar <ismetacar@bil.omu.edu.tr>
2018-11-18 00:55:47 +0300

    register email sender to app

--- src/__init__.py
+++ src/__init__.py
@@ -1,14 +1,28 @@
-from flask import Flask
-from pymongo import MongoClient
 import sentry_sdk
+from flask import Flask
+from flask_mail import Mail
+from pymongo import MongoClient
 
 
 def create_app(settings):
-    app = Flask(__name__)
+    app = Flask(__name__, template_folder="../templates/")
 
-    sentry_sdk.init(settings['sentry_connection_string'])
     app.db = MongoClient(settings['mongo_connection_string']).get_database(settings['default_database'])
     app.public_endpoints = ['healtcheck', 'create_token', 'site_map']
+
+    if settings['sentry']:
+        sentry_sdk.init(settings['sentry_connection_string'])
+
+    if settings['email']:
+        app.config['MAIL_SERVER'] = settings['mail_server']
+        app.config['MAIL_PORT'] = settings['mail_port']
+        app.config['MAIL_USE_TLS'] = False
+        app.config['MAIL_USE_SSL'] = True
+        app.config['MAIL_USERNAME'] = settings['mail_username']
+        app.config['MAIL_PASSWORD'] = settings['mail_password']
+
+        mail = Mail(app)
+        app.mail = mail
 
     from src.services import init_services
     init_services(app, settings)
