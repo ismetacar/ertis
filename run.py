ef786204b2117ffa224d4ba2dd307606b2cf25c4
ismetacar <ismetacar@bil.omu.edu.tr>
2018-11-17 23:38:59 +0300

    optimze imports

--- run.py
+++ run.py
@@ -1,17 +1,10 @@
-import json
 import optparse
-import os
-import traceback
-
-from flask import Response
 
 from confs.development import development_config
 from confs.local import local_config
 from confs.production import production_config
 from src import create_app
 from src.resources.security import ErtisSecurityManager
-from src.utils.errors import ErtisError
-from src.utils.json_helpers import bson_to_json, parse_boolean
 
 CONFIG_LOOKUP = {
     'local': local_config,
