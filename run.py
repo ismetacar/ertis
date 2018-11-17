fc8c3a539fb28c2b9c659a3894d52d8c9a2cc3d9
ismetacar <ismetacar@bil.omu.edu.tr>
2018-11-17 23:37:20 +0300

    error handler moved from run to additions

--- run.py
+++ run.py
@@ -38,31 +38,6 @@ app.debug = settings['debug']
 app.secret_key = settings['application_secret']
 app.port = settings['port']
 
-if settings.get('error_handler', False):
-
-    @app.errorhandler(Exception)
-    def handle_exceptions(error):
-        if isinstance(error, ErtisError):
-            response = {
-                'err_msg': error.err_msg or 'Internal error occurred',
-                'err_code': error.err_code or 'errors.internalError',
-                'context': error.context,
-                'reason': error.reason
-            }
-            status_code = error.status_code
-
-        else:
-            response = {
-                'err_msg': str(error),
-                'err_code': "errors.internalError",
-                'traceback': str(traceback.extract_stack())
-            }
-            status_code = getattr(error, 'code', 500)
-
-        return Response(
-            json.dumps(response, default=bson_to_json), status_code,
-            mimetype='application/json'
-        )
 
 if __name__ == '__main__':
     app.run(port=app.port)
