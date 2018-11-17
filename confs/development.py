87d9a255752d182c52179cf1023b4b1b67a638d8
Ismet Acar Dogan <ismetacar@Ismets-MacBook-Air.local>
2018-11-17 21:02:18 +0300

    config fixed

---
+++ confs/development.py
@@ -0,0 +1,11 @@
+development_config = {
+    "mongo_connection_string": "mongodb://localhost:27017/ertis",
+    "api_version": "v1",
+    "application_secret": "AtqVaL11EHVPEpbu2mr0yZgKXj1BnRj0",
+    "token_ttl": 60,
+    "default_database": "ertis",
+    "verify_token": False,
+    "error_handler": True,
+    "debug": True,
+    "port": 8888
+}
--- confs/local.py
+++ confs/local.py
@@ -1,10 +1,11 @@
-var = {
+local_config = {
     "mongo_connection_string": "mongodb://localhost:27017/ertis",
     "api_version": "v1",
     "application_secret": "AtqVaL11EHVPEpbu2mr0yZgKXj1BnRj0",
     "token_ttl": 60,
     "default_database": "ertis",
-    "verify_token": "false",
-    "error_handler": "true"
+    "verify_token": False,
+    "error_handler": True,
+    "debug": True,
+    "port": 8888
 }
-
